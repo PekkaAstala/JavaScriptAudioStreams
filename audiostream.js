b4c79cbdcba7facfab2867efd7553a623ceb7fe9
Pekka Astala <pekka.astala@sysart.fi>
2016-09-08 00:24:32 +0300

    Added config ui and stream from it

--- audiostream.js
+++ audiostream.js
@@ -1,12 +1,19 @@
-function audioElementAnalyzer(audioElement, presicion, smoothness) {
+function audioElementAnalyzer(audioElement, config) {
+
     let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     let source = audioCtx.createMediaElementSource(audioElement);
     let analyser = audioCtx.createAnalyser();
     source.connect(analyser);
-    analyser.fftSize = Math.pow(2, precision);
-    analyser.smoothingTimeConstant = smoothness;
+    analyser.fftSize = Math.pow(2, 9);
+    analyser.smoothingTimeConstant = 0.8;
     let bufferLength = analyser.frequencyBinCount;
     analyser.connect(audioCtx.destination);
+
+    config.subscribe(x => {
+        analyser.fftSize = Math.pow(2, x.precision);
+        analyser.smoothingTimeConstant = x.smoothness;
+        bufferLength = analyser.frequencyBinCount;
+    });
 
     return function() {
         var dataArray = new Uint8Array(bufferLength);
